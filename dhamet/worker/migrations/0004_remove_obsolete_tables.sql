bcb7402bdfd2d1bfe1259d1c66c481aea5f4c398
ouglsoft <ouglsoft06@gmail.com>
2026-07-18 19:53:14 +0300

    Delete dhamet directory

--- dhamet/worker/migrations/0004_remove_obsolete_tables.sql
+++
@@ -1,5 +0,0 @@
-DROP TRIGGER IF EXISTS trg_training_records_insert_meta;
-DROP TRIGGER IF EXISTS trg_training_records_delete_meta;
-DROP INDEX IF EXISTS idx_training_records_export;
-DROP TABLE IF EXISTS training_queue_meta;
-DROP TABLE IF EXISTS training_records;
